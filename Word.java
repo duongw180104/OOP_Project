0cf9bde0a49b0b5d1d4663b8ae3292f504e7d70e
hieu <144638444+doanngochieu@users.noreply.github.com>
2023-11-24 22:58:14 +0700

    Update Word.java

--- Word.java
+++ Word.java
@@ -5,8 +5,8 @@ class Word implements Serializable {
     private String word_explain;
 
     public Word(String word_target, String word_explain) {
-        this.word_target = word_target;
-        this.word_explain = word_explain;
+        this.word_target = this.word_target;
+        this.word_explain = this.word_explain;
     }
 
     public String getWordTarget() {
