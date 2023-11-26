112310d829f4cc5987d17891f953baf1ffd6c0c4
hieu <144638444+doanngochieu@users.noreply.github.com>
2023-11-26 22:54:31 +0700

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -1,41 +1,10 @@
 import java.util.ArrayList;
-import java.util.Collections;
 
-class Dictionary {
-    private ArrayList<Word> words;
-
-    public Dictionary() {
-        words = new ArrayList<>();
-    }
-
-    public void addWord(Word word) {
-        words.add(word);
-    }
-
-    public void showAllWords() {
-        Collections.sort(words, (Word word1, Word word2) -> word1.getWordTarget().compareToIgnoreCase(word2.getWordTarget()));
-        for (int i = 0; i < words.size(); i++) {
-            System.out.printf("%d | %s | %s\n", i + 1, words.get(i).getWordTarget(), words.get(i).getWordExplain());
-        }
-    }
-
-    public Word findWordByWordTarget(String word_target) {
-        for (Word word : words) {
-            if (word.getWordTarget().equals(word_target)) {
-                return word;
-            }
-        }
-        return null;
-    }
-
-    public void removeWord(String target) {
-        for (Word word : words) {
-            if (word.getWordTarget().equals(target)) {
-                words.remove(word);
-                System.out.println("Word removed successfully.");
-                return;
-            }
-        }
-        System.out.println("Word not found in the dictionary.");
+public class Dictionary extends Word{
+    public ArrayList<Word> wordList = new ArrayList<Word>();
+    public void addWord(String word1, String word2) {
+        Word w = new Word();
+        w.setWord(word1, word2);
+        this.wordList.add(w);
     }
 }
